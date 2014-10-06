a088e89588621e6dea3a77b64067688abc111d25
Daren-Ken St-laurent <daren511@hotmail.com>
2014-10-06 13:22:47 -0400

    tes

---
+++ TP2/FORM_Rapport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2
+{
+    public partial class FORM_Rapport : Form
+    {
+        public FORM_Rapport()
+        {
+            InitializeComponent();
+        }
+
+        // ---------- VARIABLES
+        public SqlConnection conn = null;
+        public int codeRapport = -1;
+        // codeRapport: 0 -- Fournisseurs
+        //              1 -- Fournisseurs (publipostage)
+        //              2 -- Fournisseurs (produits)
+        //              3 -- Fournisseurs (quantité minimum)
+    }
+}

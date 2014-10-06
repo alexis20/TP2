55ba9e6dc2cc31c9f17b1b1e125e14a6d7fd3b80
Alexis Lalonde <ale_borg-18@hotmail.com>
2014-10-06 10:17:51 -0400

    TSMI_Rapports

--- TP2/FORM_Main.cs
+++ TP2/FORM_Main.cs
@@ -65,7 +65,8 @@ namespace TP2
             if (conn.State.ToString() == "Open")
             {
                 TSMI_Connexion.Enabled = false;
-                TSMI_Deconnexion.Enabled = true;                
+                TSMI_Deconnexion.Enabled = true;
+                TSMI_Rapports.Enabled = true;
             }
         }
 
@@ -136,6 +137,7 @@ namespace TP2
         {
             TSMI_Deconnexion.Enabled = false;
             TSMI_Connexion.Enabled = true;
+            TSMI_Rapports.Enabled = false;
             DGV_Inventaire.DataSource = null;
             DGV_Fournisseur.DataSource = null;
             DGV_QTE_Minimum.DataSource = null;
@@ -489,6 +491,26 @@ namespace TP2
                 ReloadDGVInventaire(TB_RECHERCHE_Inventaire.Text);
             else
                 ReloadDGVInventaire();
+        }
+
+        private void TSMI_LISTER_FOURN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TSMI_LISTER_FOURN_Pub_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TSMI_LISTER_FOURN_Prod_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TSMI_LISTER_FOURN_QteMin_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
